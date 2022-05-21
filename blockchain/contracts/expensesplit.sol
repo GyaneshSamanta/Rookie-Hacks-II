978c3b241b81903bf108ae4dab5221e61a2fee45
Gyanesh Samanta <mail.gyaneshsamanta@gmail.com>
2022-05-22 00:45:33 +0530

    ✨ Update Smartcontract

--- blockchain/contracts/expensesplit.sol
+++ blockchain/contracts/expensesplit.sol
@@ -3,17 +3,21 @@ pragma solidity ^0.8.0;
 contract Splitwise{
       mapping(address => mapping (address => int32)) public debts;
       address[] public users;
-      
+      int[] balance_logs;
+      int[] deletion_logs;
       function lookup(address debtor, address creditor) public view returns (int32 ret){
           ret = debts[debtor][creditor];
       }
+
       function add_IOU(address creditor, int32 amount) public{
           addDebt(msg.sender, creditor, amount);
+          balance_logs.push(amount); 
       }
       function addDebt(address debtor, address creditor , int32 amount) public{
           debts[debtor][creditor] += amount;
           addToUsers(creditor);
           addToUsers(debtor);
+          balance_logs.push(amount);
       }
       function addToUsers(address add) private{
           for (uint i = 0; i < users.length; i++){
