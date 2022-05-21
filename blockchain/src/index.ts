a58c7f1fcb00ca240d2b0e94889a7fec35317362
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2022-05-21 23:49:43 +0530

    ✨ add build tooling

---
+++ blockchain/src/index.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+import fs from "fs/promises";
+import { join } from "path";
+import { spawnSync } from "child_process";
+
+const main = async () => {
+  const list = await fs.readdir(join(__dirname, "..", "contracts"));
+  const answers = await inquirer.prompt([
+    {
+      type: "list",
+      name: "contractname",
+      message: "Which solidity file do you want to compile?",
+      choices: list,
+    },
+  ]);
+  const solc = spawnSync(
+    "solcjs",
+    [
+      "--bin",
+      `blockchain/contracts/${answers.contractname}`,
+      "--output-dir",
+      "blockchain/build",
+    ],
+    {
+      stdio: [process.stdin, process.stdout, process.stderr],
+    }
+  );
+};
+
+main()
+  .then(() => {
+    console.log("Script executed successfully at: ", new Date().toISOString());
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.log("Script executed with errors!");
+    console.error(err);
+    process.exit(1);
+  });
