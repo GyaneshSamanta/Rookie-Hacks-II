0f3cdd6cc67bb1ea03406f4988e20bf8a22d057f
jagnani73 <yashjagnani73@gmail.com>
2022-05-22 08:19:43 +0530

    ✨ client: add/delete/edit static

--- client/components/dashboard/dashboard-card.tsx
+++ client/components/dashboard/dashboard-card.tsx
@@ -1,18 +1,17 @@
 import { useMemo, useState } from "react";
+import { Form, Formik } from "formik";
 
 import { ParseAssociationID } from "../../utils/functions";
 import { DashboardCardProps } from "../../utils/interfaces/dashboard-interfaces";
-import { Modal } from "../shared";
+import { Input, Modal } from "../shared";
 
 const DashboardCard: React.FC<DashboardCardProps> = ({ association }) => {
   const [modal, setModal] = useState<boolean>(false);
 
   const [transactions] = Object.values(association);
+  const [association_id] = Object.keys(association);
 
-  const usernames = useMemo(
-    () => ParseAssociationID(Object.keys(association)[0]),
-    []
-  );
+  const usernames = useMemo(() => ParseAssociationID(association_id), []);
 
   const amount = useMemo(() => {
     var amountSum: number = 0;
@@ -21,6 +20,13 @@ const DashboardCard: React.FC<DashboardCardProps> = ({ association }) => {
     });
     return amountSum;
   }, []);
+
+  const deleteTransactionHandler = (
+    association_id: string,
+    transaction_id: string
+  ) => {
+    console.log(association_id, transaction_id);
+  };
 
   return (
     <>
@@ -47,16 +53,69 @@ const DashboardCard: React.FC<DashboardCardProps> = ({ association }) => {
 
             <div className="overflow-auto h-full">
               {transactions.map((transaction) => (
-                <div className="flex justify-between py-1 border-b-2">
-                  <p className="">{transaction.name}</p>
-                  <p className="pl-4 mt-auto">{transaction.amount}</p>
-                </div>
+                <Formik
+                  key={transaction.trans_ID}
+                  onSubmit={(values) => {
+                    console.log(values);
+                    setModal(false);
+                  }}
+                  initialValues={{ [transaction.trans_ID]: transaction.amount }}
+                >
+                  {({ initialValues, values }) => (
+                    <Form className="py-1 border-b-2">
+                      <div className="flex justify-between">
+                        <p className="">{transaction.name}</p>
+
+                        <div className="flex items-center">
+                          <Input
+                            id={transaction.trans_ID}
+                            name={transaction.trans_ID}
+                            type="number"
+                            classNames={{
+                              wrapper: "w-24 pl-4 mt-auto",
+                              input: "w-full text-right",
+                            }}
+                            placeholder="Amount"
+                          />
+                          <svg
+                            stroke="currentColor"
+                            fill="currentColor"
+                            stroke-width="0"
+                            viewBox="0 0 20 20"
+                            className="w-6 text-red-500 cursor-pointer"
+                            onClick={() =>
+                              deleteTransactionHandler(
+                                association_id,
+                                transaction.trans_ID
+                              )
+                            }
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </div>
+                      </div>
+
+                      {initialValues[transaction.trans_ID] !==
+                        values[transaction.trans_ID] && (
+                        <button
+                          type="submit"
+                          className="px-2 py-1 bg-hedera-blue text-white rounded text-sm flex mx-auto"
+                        >
+                          Save
+                        </button>
+                      )}
+                    </Form>
+                  )}
+                </Formik>
               ))}
-              <small className="text-right block mt-2">
-                TOTAL: <span className="text-base font-medium">{amount}</span>
-              </small>
             </div>
           </div>
+          <button type="submit">save</button>
         </Modal>
       )}
 
--- client/components/dashboard/dashboard-hero.tsx
+++ client/components/dashboard/dashboard-hero.tsx
@@ -1,29 +1,124 @@
+import { useState } from "react";
+
 import { DashboardHeroProps } from "../../utils/interfaces/dashboard-interfaces";
 import { DashboardCard } from ".";
+import { Form, Formik } from "formik";
+import { Input, Modal } from "../shared";
 
 const DashboardHero: React.FC<DashboardHeroProps> = ({ associations }) => {
+  const [modal, setModal] = useState<boolean>(false);
+
   return (
-    <section>
-      <div className="bg-hedera-secondary text-white py-36 px-48">
-        <h1 className="text-5xl font-bold">Welcome [name]</h1>
-        <h6 className="text-hedera-purple text-xl font-light mt-8">
-          Lets see something
-        </h6>
-      </div>
+    <>
+      {modal && (
+        <Modal
+          className="bg-white overflow-hidden pt-0 w-96 rounded-lg border"
+          toggleModal={
+            setModal as React.Dispatch<React.SetStateAction<boolean>>
+          }
+        >
+          <Formik
+            onSubmit={(values) => {
+              console.log(values);
+              setModal(false);
+            }}
+            initialValues={{}}
+          >
+            {() => (
+              <Form className="p-4">
+                <p className="text-xl text-hedera-blue font-small-caps font-semibold mb-2">
+                  Add a new transaction
+                </p>
 
-      <p className="bg-gradient-to-r text-center text-xl text-white py-10 hedera-primary font-medium">
-        Nulla veniam irure commodo duis officia aliquip irure ea nostrud.
-      </p>
+                <div className="grid grid-cols-1 gap-y-2 items-center justify-between">
+                  <Input
+                    id="transaction_name"
+                    name="transaction_name"
+                    type="text"
+                    placeholder="Username"
+                    classNames={{
+                      wrapper: "mx-auto",
+                      input:
+                        "border-b-2 focus:border-hedera-secondary transition-all",
+                    }}
+                  />
+                  <Input
+                    id="transaction_name"
+                    name="transaction_name"
+                    type="text"
+                    placeholder="Transaction Name"
+                    classNames={{
+                      wrapper: "mx-auto",
+                      input:
+                        "border-b-2 focus:border-hedera-secondary transition-all",
+                    }}
+                  />
+                  <Input
+                    id="transaction_amount"
+                    name="transaction_amount"
+                    type="number"
+                    placeholder="Transaction Amount"
+                    classNames={{
+                      wrapper: "mx-auto",
+                      input:
+                        "border-b-2 focus:border-hedera-secondary transition-all",
+                    }}
+                  />
+                </div>
 
-      <div className="grid grid-cols-3 min-w-fit w-10/12 mx-auto mt-20 justify-center items-start">
-        {associations.map((association) => (
-          <DashboardCard
-            key={Object.keys(association)[0]}
-            association={association}
-          />
-        ))}
-      </div>
-    </section>
+                <button
+                  type="submit"
+                  className="px-4 py-2 mt-4 bg-hedera-purple text-white rounded text-sm flex mx-auto"
+                >
+                  Save
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </Modal>
+      )}
+
+      <section>
+        <div className="bg-hedera-secondary text-white py-36 px-48">
+          <h1 className="text-5xl font-bold">Welcome [name]</h1>
+          <h6 className="text-hedera-purple text-xl font-light mt-8">
+            Lets see something
+          </h6>
+        </div>
+
+        <div
+          className="bg-gradient-to-r text-center text-xl text-white py-10 hedera-primary font-medium flex items-center justify-evenly cursor-pointer"
+          onClick={() => setModal(true)}
+        >
+          <p>
+            Nulla veniam irure commodo duis
+            <br />
+            officia aliquip irure ea nostrud.
+          </p>
+
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 24 24"
+            className="w-20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M16 2H8C4.691 2 2 4.691 2 8v13a1 1 0 0 0 1 1h13c3.309 0 6-2.691 6-6V8c0-3.309-2.691-6-6-6zm4 14c0 2.206-1.794 4-4 4H4V8c0-2.206 1.794-4 4-4h8c2.206 0 4 1.794 4 4v8z" />
+            <path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4z" />
+          </svg>
+        </div>
+
+        <div className="grid grid-cols-3 min-w-fit w-10/12 mx-auto mt-20 justify-center items-start">
+          {associations.map((association) => (
+            <DashboardCard
+              key={Object.keys(association)[0]}
+              association={association}
+            />
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
