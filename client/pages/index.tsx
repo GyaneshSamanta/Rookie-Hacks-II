6a515c43db756fa01c116ee9ed0ebbdf26bc1076
jagnani73 <yashjagnani73@gmail.com>
2022-05-22 10:42:27 +0530

    🚀 client: fix

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,18 +1,17 @@
-import type { GetStaticPropsResult, NextPage } from "next";
+import { useEffect } from "react";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
 
 import { ROUTES } from "../utils/constants/shared-constants";
 
 const HomePage: NextPage = () => {
+  const { replace } = useRouter();
+
+  useEffect(() => {
+    replace(ROUTES.SIGNUP);
+  }, []);
+
   return <></>;
 };
 
 export default HomePage;
-
-export async function getStaticProps(): Promise<GetStaticPropsResult<{}>> {
-  return {
-    redirect: {
-      destination: ROUTES.SIGNUP,
-      permanent: true,
-    },
-  };
-}
