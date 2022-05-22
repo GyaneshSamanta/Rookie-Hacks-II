8af9f8d68659b375801ece4e5af5d4815a6c1444
jagnani73 <yashjagnani73@gmail.com>
2022-05-22 09:18:56 +0530

    🚨 client: camel case css props

--- client/components/dashboard/dashboard-hero.tsx
+++ client/components/dashboard/dashboard-hero.tsx
@@ -99,7 +99,7 @@ const DashboardHero: React.FC<DashboardHeroProps> = ({ associations }) => {
           <svg
             stroke="currentColor"
             fill="currentColor"
-            stroke-width="0"
+            strokeWidth="0"
             viewBox="0 0 24 24"
             className="w-20"
             xmlns="http://www.w3.org/2000/svg"
